cceaf4b47b117e8b2ef9fc918d21c6b7cf0fcab9
armen <arakelyan2001@mail.ru>
2024-11-09 17:31:11 +0400

    make container

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -8,7 +8,7 @@ function Counter() {
       <button onClick={() => setCount((count) => count + 1)}>
         Count is {count}
       </button>
-      <p>Edit <code>src/components/Counter.jsx</code> and save to test HMR</p>
+      <p>Edit <code>src/components/Counter.jsxxx</code> and save to test HMR</p>
     </div>
   );
 }
